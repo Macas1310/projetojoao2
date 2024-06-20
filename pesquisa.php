7fdd8b0124642d28276b4586ec7f298f7e6c50b6
Macario1310 <159207820+Macas1310@users.noreply.github.com>
2024-06-19 22:12:50 -0300

    .....................

---
+++ pesquisa.php
@@ -0,0 +1,46 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bd_cadastro";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error){
+    die("Erro na conexao com o banco de dados: " . $conn->connect_error);
+}
+
+$termo = $_GET["termo"];
+
+$sql = "SELECT * FROM clientes WHERE nome LIKE '%$termo%' OR email LIKE '%$termo%' OR telefone LIKE '%$termo%' OR descricao LIKE '%$termo%'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0){
+    echo "<h2>Resultados da Pesquisas</h2>";
+    echo "<table border='1'";
+    echo "<tr><th>Nome</th><th>Email</th><th>Telefone</th><th>Descrição</th></tr>";
+    while($row = $result->fetch_assoc()){
+        echo "<tr><td>".$row["nome"]."<tr><td>".$row["email"]."<tr><td>".$row["telefone"]."<tr><td>".$row["descricao"]."</td></tr>";
+    }
+    echo "</table>";
+}else {
+    echo "Nenhum resultado encontrado.";
+}
+
+$conn->close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
